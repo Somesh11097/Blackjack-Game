26367f791640a5dad92be2e8b201b6f8ef95a699
Somesh banerjee <32162479+Somesh11097@users.noreply.github.com>
2022-08-20 15:58:03 +0530

    commit
    start with blackjack game logic

---
+++ app.js
@@ -0,0 +1,24 @@
+// let fCard = 6
+// let sCard = 5
+// let sum = fCard + sCard
+
+
+
+// if (sum < 21){
+//     console.log("Do you want to draw a new card")
+
+// } else if(sum === 21){
+//     console.log("The player has blackjack")
+// }
+// else ( sum> 21){
+//     console.log("You lost");
+// }
+
+let age =  21
+
+if (age >= 21){
+    console.log("Welcome");
+}
+else{
+    console.log("Bye bye");
+}
